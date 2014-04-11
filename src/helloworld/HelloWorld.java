b4d50df5b02f2b5200059eebe87d01c190bbdf45
g12934nm <g12934nm@s30433.tsuda.ac.jp>
2014-04-11 11:03:36 +0900

    新規にHelloWorldを作成しました

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12934nm
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("HelloWorld");
+        // TODO code application logic here
+    }
+}
